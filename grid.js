b3275c403f767eb5637b08fbf16e36ac6817de5c
MaxDublenko <55203253+MaxDublenko@users.noreply.github.com>
2020-09-30 15:20:07 -0600

    Add files via upload

--- grid.js
+++ grid.js
@@ -1,8 +1,15 @@
+// When it's player 1's turn, turn = 1, and for player 2, turn = 2
+let turn = 1;
+let square = 0;
+
 function createGridArray() {
   // Create and return a grid array
-  return [ [0, 0, 0],
-           [0, 0, 0],
-           [0, 0, 0] ];
+  board = [ [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0] ];
+
+
+  return board;
 }
 
 function createDivGrid(grid) {
@@ -10,11 +17,6 @@ function createDivGrid(grid) {
     for (let col = 0; col < NUM_COLS; col++) {
       // Create a div for each element in 2D grid
       let divEl = document.createElement("div");
-      if (grid[row][col] === 1) {
-        divEl.classList.add("blue");
-      } else if (grid[row][col] === -1) {
-        divEl.classList.add("red");
-      }
       if (row === 0) {
         divEl.style.borderTop = '1px solid transparent';
       } else if (row === 2) {
@@ -29,5 +31,19 @@ function createDivGrid(grid) {
       // Add div to container
       document.getElementById('container').append(divEl);
     }
+  }
+}
+
+// Function to update the grid after a change has been made
+function updateGrid(grid, squareNum) {
+  // Accessing all elements of container div
+  var elements = document.getElementById('container').children;
+
+  // Player 1 is blue, so if the turn is 1 then make the square player 1 clicks blue
+  if (turn === 1) {
+    // Have to substract 1 from squareNum because arrays start at 0, not 1
+    elements.item(squareNum - 1).classList.add('blue');
+  } else {
+    elements.item(squareNum - 1).classList.add('red');
   }
 }
--- main.js
+++ main.js
@@ -4,9 +4,6 @@
 const NUM_ROWS = 3;
 const NUM_COLS = 3;
 
-// Ideas for what to add are at the bottom of this file (only if you have no ideas)
-// Some functions you should add are at the bottom of this file
-
 // Create an array to represent a grid
 let grid = createGridArray();
 
@@ -15,65 +12,51 @@ createDivGrid(grid);
 
 // In the grid, 1 represents X, -1 represents O, and 0 represents an empty tile
 
+// Getting mouse coordinates
 
+var x = 0;
+var y = 0;
 
+function showCoords(event) {
+  x = event.clientX;
+  y = event.clientY;
+  // We now need to substract the margins so (0, 0) is the tic tac toe div's top left corner
+  y = y - 60 // 60 is the top margin
+  x = x - (screen.width - 600) / 2 // 600 is the tic tac toe div width
+        // ^^This returns the left margin. It is divided by two so it does not give the sum of both margins.
+  // Make sure Developer tools is closed when testing / playing
+  // The maximum value for both X and Y is 599, or 600-1
+  document.getElementById("coordinates").innerHTML = "X coords: " + x + ", Y coords: " + y;
+}
 
+// Function to check what square is being clicked
+document.getElementById('container').addEventListener('click', move);
 
+function move() {
+  if (x < 200 && y < 200 && x > 0 && y > 0) {
+    updateGrid(grid, 1);
+  } else if (x < 400 && y < 200 && x > 200 && y > 0) {
+    updateGrid(grid, 2);
+  } else if (x < 600 && y < 200 && x > 400 && y > 0) {
+    updateGrid(grid, 3);
+  } else if (x < 200 && y < 400 && x > 0 && y > 200) {
+    updateGrid(grid, 4);
+  } else if (x < 400 && y < 400 && x > 200 && y > 200) {
+    updateGrid(grid, 5);
+  } else if (x < 600 && y < 400 && x > 400 && y > 200) {
+    updateGrid(grid, 6);
+  } else if (x < 200 && y < 600 && x > 000 && y > 400) {
+    updateGrid(grid, 7);
+  } else if (x < 400 && y < 600 && x > 200 && y > 400) {
+    updateGrid(grid, 8);
+  } else if (x < 600 && y < 600 && x > 400 && y > 400) {
+    updateGrid(grid, 9);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* End of file */
+  // Switch player
+  if (turn === 1) {
+    turn++;
+  } else {
+    turn--;
+  }
+}
